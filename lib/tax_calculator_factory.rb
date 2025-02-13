9e8e9cea40ffc8547ccdcf848071e48e0ab61fbe
justinjkim <jjkim928@gmail.com>
2025-02-12 21:36:36 -0500

    add formatting changes and point to proper relative files
    This commit doesn't have any significant code change, it's really just
    a code formatting issue. I also realized I forgot to add the
    require statements at the top, thus making my tests fail.

--- lib/tax_calculator_factory.rb
+++ lib/tax_calculator_factory.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
+require_relative 'tax_calculators/basic_sales_tax_calculator'
+require_relative 'tax_calculators/import_tax_calculator'
 class TaxCalculatorFactory
   EXEMPT_FROM_BASIC_SALES_TAX_CATEGORIES = [:book, :food, :medical]
 
   def self.for_item(category:, imported:)
     calculators = []
-    calculators << BasicSalesTaxCalculator.new unless EXEMPT_CATEGORIES.include?(category)
-    calculators << ImportTaxCalculator.new if imported
+    calculators <<
+      TaxCalculators::BasicSalesTaxCalculator.new unless EXEMPT_FROM_BASIC_SALES_TAX_CATEGORIES.include?(category)
+    calculators << TaxCalculators::ImportTaxCalculator.new if imported
     calculators
   end
 end
