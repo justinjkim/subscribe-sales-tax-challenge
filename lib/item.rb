600fc192c672055641deb638f31d480319b2e164
justinjkim <jjkim928@gmail.com>
2025-02-12 21:06:14 -0500

    add tax calculation logic
    This is the hardest part that took me the longest to understand and
    figure out. I definitely had to ask Claude.ai for help, especially for
    the part of rounding to the nearest 0.05. But after studying the sample
    input and outputs, it seemed that the basic sales tax + import duty tax
    could be combined together, and then that final combined rate would
    be multiplied against the base price to get that initial number that would
    then get rounded.
    
    In terms of why the rounding required going up to the nearest 0.05, the
    AI said it's because you can divide 100 by 5 exactly 20 times. I kind of
    get it, but it definitely still threw me in for a loop. Like I said,
    I'm not a math person :D.

--- lib/item.rb
+++ lib/item.rb
@@ -8,6 +8,7 @@ class Item
     :category,
     :imported
   )
+
   def initialize(name:, quantity:, price:, category:, imported: false)
     @name = name
     @quantity = quantity
@@ -20,5 +21,21 @@ class Item
         category: category,
         imported: imported,
       )
+  end
+
+  def total_tax
+    total_rate = @tax_calculators.sum(&:rate)
+    tax = price * (total_rate/100.0)
+    round_to_nearest_five_cents(tax) * quantity
+  end
+
+  def total_price
+    (price * quantity) + total_tax
+  end
+
+  private
+
+  def round_to_nearest_five_cents(amount)
+    (amount * 20).ceil / 20.0
   end
 end
